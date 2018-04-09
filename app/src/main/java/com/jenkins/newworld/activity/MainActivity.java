7040f87087f757fbf3f93772b99c1a5ba0629bc5
jenkins-chou <13413607283@163.com>
2018-04-09 16:11:28 +0800

    2018-04-09

---
+++ app/src/main/java/com/jenkins/newworld/activity/MainActivity.java
@@ -0,0 +1,18 @@
+package com.jenkins.newworld.activity;
+
+import android.app.Activity;
+import android.os.Build;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.WindowManager;
+
+import com.jenkins.newworld.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
---
+++ app/src/main/java/com/jenkins/newworld/util/Common.java
@@ -0,0 +1,12 @@
+package com.jenkins.newworld.util;
+
+import android.app.Activity;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by zhouzhenjian on 2018/4/8.
+ */
+
+public class Common {
+}
