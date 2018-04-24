af796a2b5512de968f0f8392581327539a07d613
jenkins-chou <13413607283@163.com>
2018-04-24 15:47:24 +0800

    2018-04-24 更新启动页面，添加关注页面

--- app/src/main/java/com/jenkins/newworld/fragment/main/FragAttention.java
+++ app/src/main/java/com/jenkins/newworld/fragment/main/FragAttention.java
@@ -9,7 +9,13 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
 
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.load.engine.DiskCacheStrategy;
 import com.jenkins.newworld.R;
+
+import org.raphets.roundimageview.RoundImageView;
+
+import butterknife.BindView;
 import butterknife.ButterKnife;
 
 /**
@@ -17,13 +23,28 @@ import butterknife.ButterKnife;
  */
 
 public class FragAttention extends Fragment {
-
+    @BindView(R.id.frag_attention_g1)
+    RoundImageView frag_attention_g1;
+    @BindView(R.id.frag_attention_g2)
+    RoundImageView frag_attention_g2;
+    @BindView(R.id.frag_attention_g3)
+    RoundImageView frag_attention_g3;
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.frag_main_attention,container,false);
         ButterKnife.bind(this,view);
+        initGif();
         return view;
+    }
+
+    public void initGif(){
+        Glide.with(this).load(R.mipmap.frag_attention_g1)
+                .diskCacheStrategy(DiskCacheStrategy.ALL).into(frag_attention_g1);
+        Glide.with(this).load(R.mipmap.frag_attention_g2)
+                .diskCacheStrategy(DiskCacheStrategy.ALL).into(frag_attention_g2);
+        Glide.with(this).load(R.mipmap.frag_attention_g4)
+                .diskCacheStrategy(DiskCacheStrategy.ALL).into(frag_attention_g3);
     }
 
 }
